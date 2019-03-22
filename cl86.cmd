68d05bfcdaaf377b81c261ae708889a3716b67f4
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-03-22 15:01:24 -0500

    Command for Visual Studio build tools

---
+++ cl86.cmd
@@ -0,0 +1,2 @@
+rc res\tinyTerm.rc
+cl -Ox /DUNICODE /I..\x86\include tiny.c term.c host.c ssh2.c auto_drop.c res\tinyTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib winmm.lib bcrypt.lib crypt32.lib shlwapi.lib ucrt.lib ..\x86\lib\libssh2.lib /link /NODEFAULTLIB:MSVCRT /NODEFAULTLIB:LIBUCRT /out:tinyTerm.exe
