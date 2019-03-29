2e44bcfc16230a799c856d302249c715f11680d2
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-03-29 17:22:19 -0500

    Update cl64.cmd

--- cl64.cmd
+++ cl64.cmd
@@ -1,2 +1,2 @@
 rc res\tinyTerm.rc
-cl -Ox /DUNICODE /I..\x64\include tiny.c term.c host.c ssh2.c auto_drop.c res\tinyTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib winmm.lib bcrypt.lib crypt32.lib shlwapi.lib ucrt.lib ..\x64\lib\libssh2.lib /link /NODEFAULTLIB:MSVCRT /NODEFAULTLIB:LIBUCRT /out:tinyTerm64.exe
+cl -Ox /DUNICODE /I..\x64\include tiny.c term.c host.c ssh2.c auto_drop.c res\tinyTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib winmm.lib bcrypt.lib crypt32.lib shlwapi.lib ..\x64\lib\libssh2.lib /MD /link /out:tinyTerm64.exe
