a7e37cbaa53a6bf1f485a5da5eb951e31488e252
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-03-31 18:07:00 -0500

    Delete cl86.cmd

--- cl86.cmd
+++
@@ -1,2 +0,0 @@
-rc res\tinyTerm.rc
-cl -Ox /DUNICODE /I..\x86\include tiny.c term.c host.c ssh2.c auto_drop.c res\tinyTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib winmm.lib bcrypt.lib crypt32.lib shlwapi.lib ..\x86\lib\libssh2.lib /MD /link /out:tinyTerm.exe
