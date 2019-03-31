0a581313755df7cd4243ab4b88e9e7cc83fd1c2d
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-03-31 18:06:19 -0500

    Delete cl64.cmd

--- cl64.cmd
+++
@@ -1,2 +0,0 @@
-rc res\tinyTerm.rc
-cl -Ox /DUNICODE /I..\x64\include tiny.c term.c host.c ssh2.c auto_drop.c res\tinyTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib winmm.lib bcrypt.lib crypt32.lib shlwapi.lib ..\x64\lib\libssh2.lib /MD /link /out:tinyTerm64.exe
