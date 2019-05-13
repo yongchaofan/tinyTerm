12b19f8bf8c6246f43176d6c0e26a363cd47f7e5
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-05-12 23:25:24 -0500

    Rename vc_cl.cmd to make.cmd

--- vc_cl.cmd
+++
@@ -1,5 +0,0 @@
-rc res\tinyTerm.rc
-
-cl /c -Ox /GL /DUNICODE /I../%Platform%/include tiny.c term.c host.c ssh2.c auto_drop.c
-
-link /LTCG /NODEFAULTLIB:libucrt.lib ucrt.lib tiny.obj term.obj host.obj ssh2.obj auto_drop.obj res\tinyTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib winmm.lib bcrypt.lib crypt32.lib shlwapi.lib ../%Platform%/lib/libssh2.lib /out:tinyTerm_%Platform%.exe
